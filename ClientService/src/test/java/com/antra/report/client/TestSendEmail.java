9ed5518a80edaa425df039e1859019c3d93c96b6
ZONGCHANG LYU <zongchanglyu@zongchangdembp.attlocal.net>
2021-05-09 20:07:33 -0700

    first commit and also update delete functionality

---
+++ ClientService/src/test/java/com/antra/report/client/TestSendEmail.java
@@ -0,0 +1,28 @@
+package com.antra.report.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestSendEmail {
+
+    @Autowired
+    private QueueMessagingTemplate queueMessagingTemplate;
+
+
+    @Test
+    public void testSendingEmail() {
+        Map<String, String> message = new HashMap<>();
+        message.put("to", "zongchanglyu@gmail.com");
+        message.put("from", "zongchal@uci.edu");
+        message.put("subject", "Test Email");
+        message.put("body", "I did it");
+        message.put("token", "12345");
+        queueMessagingTemplate.convertAndSend("email_queue", message);
+    }
+}
